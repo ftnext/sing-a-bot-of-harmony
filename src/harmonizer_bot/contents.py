3aec40d00e75ee420da817c126032578c8912a12
ftnext <takuyafjp+develop@gmail.com>
2022-05-22 18:08:19 +0900

    [feat] Count down Nagoya 109 cinemas!

--- src/harmonizer_bot/contents.py
+++ src/harmonizer_bot/contents.py
@@ -11,19 +11,18 @@ class Content(ABC):
 
 
 class Nagoya109CinemasContent(Content):
-    RESERVE_START = date(2022, 5, 13)
-    UNTIL_RESERVE_START = DayCountDown(RESERVE_START, include=False)
+    RUN_1ST = date(2022, 5, 28)
+    UNTIL_RUN_1ST = DayCountDown(RUN_1ST, include=False)
+    RUN_2ND = date(2022, 5, 31)
+    UNTIL_RUN_2ND = DayCountDown(RUN_2ND, include=False)
 
     def __init__(self, date_: date) -> None:
         self._date = date_
 
     def generate(self) -> str:
         text = "#アイの歌声を聴かせて 愛知の109シネマズ名古屋さんの映画祭でライブ音響上映！！\n\n"
-        text += "- 5/28(土) 16:30〜\n"
-        text += "- 5/31(火) 16:35〜\n\n"
-        text += (
-            f"チケットは、{self.UNTIL_RESERVE_START(self._date)}日後の"
-            f"{self.RESERVE_START:%-m/%-d(%a)} 0時から発売！\n\n"
-        )
+        text += f"- 5/28(土) 16:30〜 （あと{self.UNTIL_RUN_1ST(self._date)}日！）\n"
+        text += f"- 5/31(火) 16:35〜 （あと{self.UNTIL_RUN_2ND(self._date)}日）\n\n"
+        text += "チケット発売中！🎫\n"
         text += "詳しくは https://109cinemas.net/events/liveonkyo_nagoya/ をどうぞ！"
         return text
