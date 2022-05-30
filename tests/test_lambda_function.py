c307d817963ab5bae9b6b7a6e612fdde272ed637
ftnext <takuyafjp+develop@gmail.com>
2022-05-30 20:24:49 +0900

    [test] Information

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -2,7 +2,7 @@ from datetime import date
 from textwrap import dedent
 from unittest import TestCase
 
-from lambda_function import generate_text
+from lambda_function import generate_information_text, generate_text
 
 
 class GenerateTextTestCase(TestCase):
@@ -17,5 +17,21 @@ class GenerateTextTestCase(TestCase):
         ).rstrip()
 
         actual = generate_text(date(2022, 5, 30))
+
+        self.assertEqual(actual, expected)
+
+
+class GenerateInformationTextTestCase(TestCase):
+    def test_information(self):
+        expected = dedent(
+            """\
+            アイの歌声を聴かせて 劇場で上映中！🎬 https://eigakan.org/theaterpage/schedule.php?t=ainouta
+            期間限定配信🎥は今日を含めてあと13日です(6/10まで)。
+
+            今夜はきれいなお月さまでしょうか？
+            """
+        ).rstrip()
+
+        actual = generate_information_text(date(2022, 5, 29))
 
         self.assertEqual(actual, expected)
