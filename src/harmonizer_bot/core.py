8643ab96d8064da0ae15d1507ce10e69418d2a9a
ftnext <takuyafjp+develop@gmail.com>
2022-05-31 21:32:57 +0900

    [refactor] Move TextGenerator in bot package!

--- src/harmonizer_bot/core.py
+++ src/harmonizer_bot/core.py
@@ -1,3 +1,10 @@
 class TextGenerator:
+    def __init__(self) -> None:
+        self.event_handlers = {}
+
     def register(self, event_name: str):
-        raise NotImplementedError
+        def wrapped(func):
+            self.event_handlers[event_name] = func
+            return func
+
+        return wrapped
