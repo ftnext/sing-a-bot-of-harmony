d01fd62bdba068eaf14fe0aafc2e1d0c7b61b1d5
ftnext <takuyafjp+develop@gmail.com>
2022-06-02 22:30:04 +0900

    [refactor] Use greeting content in package

--- lambda_function.py
+++ lambda_function.py
@@ -8,10 +8,12 @@ from typing import TYPE_CHECKING
 from zoneinfo import ZoneInfo
 
 from requests_oauthlib import OAuth1Session
-from sparkling_counter import DayCountDown, XthDayCount
+from sparkling_counter import DayCountDown
 
-from harmonizer_bot.contents import Nagoya109CinemasContent
-from harmonizer_bot.contents.base import Content
+from harmonizer_bot.contents import (
+    MorningGreetingContent,
+    Nagoya109CinemasContent,
+)
 from harmonizer_bot.core import TextGenerator
 
 if TYPE_CHECKING:
@@ -33,29 +35,6 @@ oauth = OAuth1Session(
 )
 
 root_generator = TextGenerator()
-
-
-class MorningGreetingContent(Content):
-    AINOUTA_XDAY_COUNT = XthDayCount(date(2021, 10, 29))
-
-    DISK_RELEASE_DAY = date(2022, 7, 27)
-    # 7/26であと1日になってほしい（翌日にはリリース）
-    DISK_RELEASE_COUNT = DayCountDown(DISK_RELEASE_DAY, include=False)
-
-    def __init__(self, date_: date) -> None:
-        self._date = date_
-
-    def generate(self) -> str:
-        text = (
-            f"{self._date:%-m/%-d}は #アイの歌声を聴かせて 公開🎬から"
-            f"{self.AINOUTA_XDAY_COUNT(self._date)}日目です。\n"
-        )
-        text += (
-            "Blu-ray&DVDリリース📀まで今日を含めて"
-            f"あと{self.DISK_RELEASE_COUNT(self._date)}日です"
-            f"({self.DISK_RELEASE_DAY:%-m/%-d}発売。現在予約期間)。\n\n"
-        )
-        return text + "今日も、元気で、頑張るぞっ、おーっ"
 
 
 @root_generator.register("morning-greeting")
