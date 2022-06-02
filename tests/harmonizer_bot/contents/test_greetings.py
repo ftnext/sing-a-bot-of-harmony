0ea16e312cfddb41f09c914000e4cb847e217060
ftnext <takuyafjp+develop@gmail.com>
2022-06-02 22:24:00 +0900

    [test] MorningGreetingContent.generate

--- tests/harmonizer_bot/contents/test_greetings.py
+++ tests/harmonizer_bot/contents/test_greetings.py
@@ -1,4 +1,5 @@
 from datetime import date
+from textwrap import dedent
 from unittest import TestCase
 from unittest.mock import MagicMock
 
@@ -14,3 +15,18 @@ class MorningGreetingContentTestCase(TestCase):
 
         self.assertIsInstance(actual, Content)
         self.assertEqual(actual._date, date_)
+
+    def test_generate(self):
+        expected = dedent(
+            """\
+            5/30は #アイの歌声を聴かせて 公開🎬から214日目です。
+            Blu-ray&DVDリリース📀まで今日を含めてあと58日です(7/27発売。現在予約期間)。
+
+            今日も、元気で、頑張るぞっ、おーっ
+            """
+        ).rstrip()
+
+        content = MorningGreetingContent(date(2022, 5, 30))
+        actual = content.generate()
+
+        self.assertEqual(actual, expected)
