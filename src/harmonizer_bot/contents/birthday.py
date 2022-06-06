bc483c9e859a15353108d9ac1f599b12d8bf50a8
ftnext <takuyafjp+develop@gmail.com>
2022-06-06 23:13:41 +0900

    [feat] Implement initializer

--- src/harmonizer_bot/contents/birthday.py
+++ src/harmonizer_bot/contents/birthday.py
@@ -1,2 +1,11 @@
-class ShionBirthdayContent:
-    pass
+from datetime import date
+
+from .base import Content
+
+
+class ShionBirthdayContent(Content):
+    def __init__(self, date_: date) -> None:
+        self._date = date_
+
+    def generate(self) -> str:
+        raise NotImplementedError
