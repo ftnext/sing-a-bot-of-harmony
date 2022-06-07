fb375b04de648a57961f186fadd4baf54acc04f2
ftnext <takuyafjp+develop@gmail.com>
2022-06-07 19:40:41 +0900

    [test] Announce June 10th みんなでアイうた

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -31,10 +31,11 @@ class GenerateInformationTextTestCase(TestCase):
     def test_information(self):
         expected = dedent(
             """\
-            アイの歌声を聴かせて 劇場で上映中！🎬 https://eigakan.org/theaterpage/schedule.php?t=ainouta
-            期間限定配信🎥は今日を含めてあと13日です(6/10まで)。
+            #アイの歌声を聴かせて 期間限定配信🎥は今日を含めてあと13日です(6/10まで)。
 
-            今夜はきれいなお月さまでしょうか？
+            📣たたーん！ 6/10(金)20:30〜 Twitterスペースで #みんなでアイうた
+            作品の感想やお気に入りのシーンなど公式先輩が募集中！詳しくは👇
+            https://twitter.com/ainouta_movie/status/1533649254425968640
             """
         ).rstrip()
 
