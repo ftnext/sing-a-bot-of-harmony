d80cfd369f0ecd1bd4d2928a2207dc565173078a
ftnext <takuyafjp+develop@gmail.com>
2022-06-11 09:16:07 +0900

    [feat] Enable to initialize the Content

--- src/harmonizer_bot/contents/events.py
+++ src/harmonizer_bot/contents/events.py
@@ -1,2 +1,11 @@
-class PlayAllTogetherContent:
-    ...
+from datetime import date
+
+from .base import Content
+
+
+class PlayAllTogetherContent(Content):
+    def __init__(self, date_: date) -> None:
+        self._date = date_
+
+    def generate(self) -> str:
+        raise NotImplementedError
