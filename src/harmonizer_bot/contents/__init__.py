f9c35b8ec97e03f50faa11434af02aed27583201
ftnext <takuyafjp+develop@gmail.com>
2022-06-11 09:52:27 +0900

    [chore] Make import shorter

--- src/harmonizer_bot/contents/__init__.py
+++ src/harmonizer_bot/contents/__init__.py
@@ -1,5 +1,6 @@
 import locale
 
+from .events import PlayAllTogetherContent  # NOQA: F401
 from .greetings import MorningGreetingContent  # NOQA: F401
 from .theaters import Nagoya109CinemasContent  # NOQA: F401
 
