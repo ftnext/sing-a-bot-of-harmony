be310b02cb4089a7f3be83212143c56e528399a4
ftnext <takuyafjp+develop@gmail.com>
2022-06-11 09:42:04 +0900

    [feat] Announce radio archive

--- src/harmonizer_bot/contents/events.py
+++ src/harmonizer_bot/contents/events.py
@@ -9,17 +9,20 @@ class PlayAllTogetherContent(Content):
     STREAMING_LAST_DAY = date(2022, 6, 10)
     # 6/10であと1日（その日が最後）になってほしい
     STREAMING_PERIOD_COUNT = DayCountDown(STREAMING_LAST_DAY, include=True)
+    RADIO_REPLAY_LAST_DAY = date(2022, 6, 17)
+    REPLAY_PERIOD_COUNT = DayCountDown(RADIO_REPLAY_LAST_DAY, include=True)
 
     def __init__(self, date_: date) -> None:
         self._date = date_
 
     def generate(self) -> str:
         text = (
-            "#アイの歌声を聴かせて 期間限定配信🎥は"
-            f"今日を含めてあと{self.STREAMING_PERIOD_COUNT(self._date)}日です"
-            f"({self.STREAMING_LAST_DAY:%-m/%-d}まで)。\n\n"
+            "#アイの歌声を聴かせて 期間限定配信🎥は終了。\n"
+            "で・す・が、6/10(金)のTwitterスペース #みんなでアイうた が楽しめます"
+            "（1週間程度とのことなので、"
+            f"今日を含めてあと{self.REPLAY_PERIOD_COUNT(self._date)}日くらい）。\n\n"
         )
-        text += "📣たたーん！ 6/10(金)20:30〜 Twitterスペースで #みんなでアイうた\n"
-        text += "作品の感想やお気に入りのシーンなど公式先輩が募集中！詳しくは👇\n"
-        text += "https://twitter.com/ainouta_movie/status/1533649254425968640"
+        text += "公式先輩、ありがとうございます❤️\n"
+        text += "もう一度ラジオを聴かせて！\n"
+        text += "https://twitter.com/ainouta_movie/status/1535260028474896384"
         return text
