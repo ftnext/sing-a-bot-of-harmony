9828f16feb630aa0c7ea1254f652e047355148ed
ftnext <takuyafjp+develop@gmail.com>
2022-06-12 15:24:48 +0900

    [test] Attempt to make tests DRYer with metaclass like test_argparse

---
+++ tests/harmonizer_bot/contents/support.py
@@ -0,0 +1,36 @@
+from datetime import date
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from harmonizer_bot.contents.base import Content
+
+
+class ContentTesterMetaclass(type):
+    """ref: https://github.com/python/cpython/blob/v3.10.5/Lib/test/test_argparse.py#L155-L266"""
+
+    def __init__(cls, name, bases, bodydict):
+        if name == "ContentTestCase":
+            return
+
+        class AddTests:
+            def __init__(self, tester_cls):
+                for test_func in [self.test_init]:
+                    test_name = test_func.__name__
+
+                    def wrapper(self, test_func=test_func):
+                        test_func(self)
+
+                    setattr(tester_cls, test_name, wrapper)
+
+            def test_init(self, tester):
+                date_ = MagicMock(spec=date)
+
+                actual = tester.target_class(date_)
+
+                tester.assertIsInstance(actual, Content)
+                tester.assertEqual(actual._date, date_)
+
+        AddTests(cls)
+
+
+ContentTestCase = ContentTesterMetaclass("ContentTestCase", (TestCase,), {})
