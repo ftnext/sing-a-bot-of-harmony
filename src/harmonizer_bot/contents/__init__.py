09a8882c615ecf6de39e73e170b064d43920cd59
ftnext <takuyafjp+develop@gmail.com>
2022-06-12 15:57:24 +0900

    [chore] Make import easier

--- src/harmonizer_bot/contents/__init__.py
+++ src/harmonizer_bot/contents/__init__.py
@@ -1,6 +1,6 @@
 import locale
 
-from .events import PlayAllTogetherContent  # NOQA: F401
+from .events import HappyProjectContent, PlayAllTogetherContent  # NOQA: F401
 from .greetings import MorningGreetingContent  # NOQA: F401
 from .theaters import Nagoya109CinemasContent  # NOQA: F401
 
