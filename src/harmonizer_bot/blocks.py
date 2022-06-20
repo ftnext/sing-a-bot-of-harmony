b53d34a303c2e264fa51d9d05a74511cd5f58219
ftnext <takuyafjp+develop@gmail.com>
2022-06-20 22:57:01 +0900

    [feat] Format Ballon with Sentences class (close #33)

--- src/harmonizer_bot/blocks.py
+++ src/harmonizer_bot/blocks.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import ClassVar
 
 
 class BasePart(ABC):
@@ -21,17 +22,28 @@ NEW_LINE = Sentence("")
 
 
 @dataclass(frozen=True)
-class Balloon(BasePart):
-    value: str
-
-    def format(self) -> str:
-        raise NotImplementedError
-
-
-@dataclass(frozen=True)
 class Sentences(BasePart):
     def __init__(self, *args) -> None:
         object.__setattr__(self, "values", args)
 
     def format(self) -> str:
         return "".join(sentence.format() for sentence in self.values).rstrip()
+
+
+@dataclass(frozen=True)
+class Balloon(BasePart):
+    value: str
+    _sentences: Sentences = field(init=False)
+    START_BALLOON: ClassVar[str] = "／"
+    END_BALLOON: ClassVar[str] = "＼"
+
+    def __post_init__(self):
+        sentences = Sentences(
+            Sentence(self.START_BALLOON),
+            Sentence(f" {self.value}"),
+            Sentence(self.END_BALLOON),
+        )
+        object.__setattr__(self, "_sentences", sentences)
+
+    def format(self) -> str:
+        return self._sentences.format()
