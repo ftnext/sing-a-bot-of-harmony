ade34b57c0f1bcdc3324c83c568184b8c2ed792b
ftnext <takuyafjp+develop@gmail.com>
2022-07-27 00:49:07 +0900

    [feat] Disk released version message (close #44)

--- src/harmonizer_bot/contents/greetings.py
+++ src/harmonizer_bot/contents/greetings.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from sparkling_counter import DayCountDown, XthDayCount
+from sparkling_counter import XthDayCount
 
 from ..blocks import NEW_LINE, Sentence, Sentences
 from .base import Content
@@ -8,10 +8,7 @@ from .base import Content
 
 class MorningGreetingContent(Content):
     AINOUTA_XDAY_COUNT = XthDayCount(date(2021, 10, 29))
-
-    DISK_RELEASE_DAY = date(2022, 7, 27)
-    # 7/26であと1日になってほしい（翌日にはリリース）
-    DISK_RELEASE_COUNT = DayCountDown(DISK_RELEASE_DAY, include=False)
+    DISK_XDAY_COUNT = XthDayCount(date(2022, 7, 27))
 
     def __init__(self, date_: date) -> None:
         self._date = date_
@@ -20,12 +17,11 @@ class MorningGreetingContent(Content):
         sentences = Sentences(
             Sentence(
                 f"{self._date:%-m/%-d}は #アイの歌声を聴かせて 公開🎬から"
-                f"{self.AINOUTA_XDAY_COUNT(self._date)}日目です。"
+                f"{self.AINOUTA_XDAY_COUNT(self._date)}日目、"
             ),
             Sentence(
-                "Blu-ray&DVDリリース📀そしてレンタル配信開始まで、今日を含めて"
-                f"あと{self.DISK_RELEASE_COUNT(self._date)}日です"
-                f"({self.DISK_RELEASE_DAY:%-m/%-d}発売。現在予約期間)。"
+                "Blu-ray&DVDリリース📀&レンタル配信開始から"
+                f"{self.DISK_XDAY_COUNT(self._date)}日目です。"
             ),
             NEW_LINE,
             Sentence("今日も、元気で、頑張るぞっ、おーっ"),
