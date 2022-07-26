3d70b77e2c61daf868096d8b53ebff2e9a04c6f8
ftnext <takuyafjp+develop@gmail.com>
2022-07-27 00:52:55 +0900

    [refactor] Simplify with metaclass

--- tests/harmonizer_bot/contents/test_greetings.py
+++ tests/harmonizer_bot/contents/test_greetings.py
@@ -1,32 +1,16 @@
 from datetime import date
-from textwrap import dedent
-from unittest import TestCase
-from unittest.mock import MagicMock
 
-from harmonizer_bot.contents.base import Content
 from harmonizer_bot.contents.greetings import MorningGreetingContent
 
+from .support import ContentTestCase
 
-class MorningGreetingContentTestCase(TestCase):
-    def test_init(self):
-        date_ = MagicMock(spec=date)
 
-        actual = MorningGreetingContent(date_)
+class MorningGreetingContentTestCase(ContentTestCase):
+    target_class = MorningGreetingContent
+    generation_date = date(2022, 7, 28)
+    generated_content = """
+    7/28は #アイの歌声を聴かせて 公開🎬から273日目、
+    Blu-ray&DVDリリース📀&レンタル配信開始から2日目です。
 
-        self.assertIsInstance(actual, Content)
-        self.assertEqual(actual._date, date_)
-
-    def test_generate(self):
-        expected = dedent(
-            """\
-            7/28は #アイの歌声を聴かせて 公開🎬から273日目、
-            Blu-ray&DVDリリース📀&レンタル配信開始から2日目です。
-
-            今日も、元気で、頑張るぞっ、おーっ
-            """
-        ).rstrip()
-
-        content = MorningGreetingContent(date(2022, 7, 28))
-        actual = content.generate()
-
-        self.assertEqual(actual, expected)
+    今日も、元気で、頑張るぞっ、おーっ
+    """
