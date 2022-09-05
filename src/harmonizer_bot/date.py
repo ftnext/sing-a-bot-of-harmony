61fd7832d840395ad1937dc7532b101faa429778
ftnext <takuyafjp+develop@gmail.com>
2022-09-06 00:56:47 +0900

    [feat] Enable to convert from date

--- src/harmonizer_bot/date.py
+++ src/harmonizer_bot/date.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
 from datetime import date
 
 
 class BirthDate(date):
     def __str__(self) -> str:
         return f"{self:%-m/%-d}"
+
+    @classmethod
+    def from_(cls, date_: date) -> BirthDate:
+        return cls(date_.year, date_.month, date_.day)
--- tests/harmonizer_bot/test_date.py
+++ tests/harmonizer_bot/test_date.py
@@ -11,3 +11,7 @@ class BirthDateTestCase(TestCase):
     def test_format_month_and_day(self):
         birth_date = BirthDate(2022, 4, 10)
         self.assertEqual(f"{birth_date}", "4/10")
+
+    def test_convert_from_date(self):
+        date_ = date(2022, 11, 20)
+        self.assertEqual(BirthDate.from_(date_), BirthDate(2022, 11, 20))
