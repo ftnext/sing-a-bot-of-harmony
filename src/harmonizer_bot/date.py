7bf5b3bd69328d889eb0250720c86f7b6563a997
ftnext <takuyafjp+develop@gmail.com>
2022-09-05 23:39:42 +0900

    [feat] String as birth date

--- src/harmonizer_bot/date.py
+++ src/harmonizer_bot/date.py
@@ -2,4 +2,5 @@ from datetime import date
 
 
 class BirthDate(date):
-    ...
+    def __str__(self) -> str:
+        return f"{self:%-m/%-d}"
--- tests/harmonizer_bot/test_date.py
+++ tests/harmonizer_bot/test_date.py
@@ -7,3 +7,7 @@ from harmonizer_bot.date import BirthDate
 class BirthDateTestCase(TestCase):
     def test_can_create(self):
         self.assertIsInstance(BirthDate(2022, 6, 6), date)
+
+    def test_format_month_and_day(self):
+        birth_date = BirthDate(2022, 4, 10)
+        self.assertEqual(f"{birth_date}", "4/10")
