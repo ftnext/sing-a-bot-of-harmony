f95b6a9ad66fd9f8d318d0bf607155592cd78547
ftnext <takuyafjp+develop@gmail.com>
2022-10-30 13:04:05 +0900

    [refactor] Introduce base date to gather common methods

--- src/harmonizer_bot/date.py
+++ src/harmonizer_bot/date.py
@@ -3,6 +3,20 @@ from __future__ import annotations
 from datetime import date
 
 
+class CustomizedBaseDate(date):
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, self.__class__):
+            return False
+        return super().__eq__(__o)
+
+    def __ne__(self, __o: object) -> bool:
+        return not self.__eq__(__o)
+
+    @classmethod
+    def from_(cls, date_: date) -> BirthDate:
+        return cls(date_.year, date_.month, date_.day)
+
+
 class BirthDate(date):
     def __str__(self) -> str:
         return f"{self:%-m/%-d}"
--- tests/harmonizer_bot/test_date.py
+++ tests/harmonizer_bot/test_date.py
@@ -1,7 +1,30 @@
 from datetime import date
 from unittest import TestCase
 
-from harmonizer_bot.date import BirthDate
+from harmonizer_bot.date import BirthDate, CustomizedBaseDate
+
+
+class CustomizedBaseDateTestCase(TestCase):
+    def test_can_create(self):
+        self.assertIsInstance(CustomizedBaseDate(2022, 6, 6), date)
+
+    def test_convert_from_date(self):
+        date_ = date(2022, 11, 20)
+        self.assertEqual(
+            CustomizedBaseDate.from_(date_), CustomizedBaseDate(2022, 11, 20)
+        )
+
+    def test_equal(self):
+        self.assertTrue(
+            CustomizedBaseDate(2022, 12, 31)
+            == CustomizedBaseDate(2022, 12, 31)
+        )
+        self.assertNotEqual(
+            CustomizedBaseDate(2022, 12, 31), date(2022, 12, 31)
+        )
+        self.assertNotEqual(
+            date(2022, 12, 31), CustomizedBaseDate(2022, 12, 31)
+        )
 
 
 class BirthDateTestCase(TestCase):
