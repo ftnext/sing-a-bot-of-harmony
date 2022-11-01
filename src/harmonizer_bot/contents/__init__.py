769f7dfa5e229834c1780c4f3b36d560a6b21a51
ftnext <takuyafjp+develop@gmail.com>
2022-11-01 22:45:13 +0900

    [chore] Tweak import

--- src/harmonizer_bot/contents/__init__.py
+++ src/harmonizer_bot/contents/__init__.py
@@ -8,6 +8,7 @@ from .greetings import MorningGreetingContent  # NOQA: F401
 from .theaters import (  # NOQA: F401
     CinemaCityContent,
     ShinjukuPiccadillyContent,
+    WowowBroadCastContent,
 )
 
 locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
