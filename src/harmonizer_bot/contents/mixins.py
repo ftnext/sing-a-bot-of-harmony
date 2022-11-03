10a492682723beaac5e751a2b214029e4d0826ef
ftnext <takuyafjp+develop@gmail.com>
2022-11-04 01:40:28 +0900

    [feat] Move logic to build schedule into mixin

--- src/harmonizer_bot/contents/mixins.py
+++ src/harmonizer_bot/contents/mixins.py
@@ -1,3 +1,5 @@
 class ScheduleBuildableMixin:
     def build_schedule(self):
-        raise NotImplementedError
+        on_and_after_schedules = self.SCHEDULES.select_on_and_after(self._date)
+        slot_to_dates_schedules = on_and_after_schedules.inverse(self._date)
+        return [str(schedule) for schedule in slot_to_dates_schedules]
