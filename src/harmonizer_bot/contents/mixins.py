7c3b4fca174d27476f7de7c5147eab2bd83f7f43
ftnext <takuyafjp+develop@gmail.com>
2022-11-04 02:03:02 +0900

    [feat] Specify window to get a part
    docs: Add type hint

--- src/harmonizer_bot/contents/mixins.py
+++ src/harmonizer_bot/contents/mixins.py
@@ -1,5 +1,10 @@
+from __future__ import annotations
+
+
 class ScheduleBuildableMixin:
-    def build_schedule(self):
+    def build_schedule(self, *, window: int | None = None):
         on_and_after_schedules = self.SCHEDULES.select_on_and_after(self._date)
+        if window:
+            on_and_after_schedules = on_and_after_schedules[:window]
         slot_to_dates_schedules = on_and_after_schedules.inverse(self._date)
         return [str(schedule) for schedule in slot_to_dates_schedules]
