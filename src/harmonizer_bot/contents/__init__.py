f6ec51d65dc12965b0b3b74ffed72999ca153243
ftnext <takuyafjp+develop@gmail.com>
2022-11-09 09:50:03 +0900

    [chore] Tweak import interface

--- src/harmonizer_bot/contents/__init__.py
+++ src/harmonizer_bot/contents/__init__.py
@@ -1,10 +1,8 @@
 import locale
 
-from .events import (  # NOQA: F401
-    DiskReleaseFestivalContent,
-    HappyProjectContent,
-)
+from .events import HappyProjectContent  # NOQA: F401
 from .greetings import MorningGreetingContent  # NOQA: F401
+from .resources import ShowingTheatersContent  # NOQA: F401
 from .theaters import (  # NOQA: F401
     CinemaCityContent,
     ShinjukuPiccadillyContent,
