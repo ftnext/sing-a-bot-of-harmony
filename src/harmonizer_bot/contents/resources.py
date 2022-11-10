b1eb9a9e81457cd3d31693709cdadd5b5d678966
ftnext <takuyafjp+develop@gmail.com>
2022-11-10 09:17:49 +0900

    [feat] Use JST time and tweak message

--- src/harmonizer_bot/contents/resources.py
+++ src/harmonizer_bot/contents/resources.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime, timedelta
+from zoneinfo import ZoneInfo
 
 from harmonizer_bot.blocks import NEW_LINE, Sentence, Sentences
 from harmonizer_bot.contents.base import Content
@@ -8,7 +9,7 @@ from harmonizer_bot.fetchers import ShowingTheatersFetcher
 class ShowingTheatersContent(Content):
     def __init__(self, date_: date) -> None:
         self._date = date_
-        self._time = datetime.now().time()
+        self._time = datetime.now(ZoneInfo("Asia/Tokyo")).time()
 
     def fetch_theaters(self):
         fetcher = ShowingTheatersFetcher()
@@ -19,7 +20,7 @@ class ShowingTheatersContent(Content):
         sentences = Sentences(
             Sentence(
                 f"アイの歌声を聴かせては以下の劇場で上映中または上映予定！"
-                f"（{self._date:%-m/%-d} {self._time:%H:%M}時点の情報です）"
+                f"（{self._date:%-m/%-d} {self._time:%H:%M}時点に取得した情報です）"
             ),
             *[Sentence(f"🌕{theater}") for theater in theaters],
             NEW_LINE,
